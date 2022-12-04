4f2d736bb02a4be5bf4dc3fd676e61a797b46b67
semihdurak34 <smhdrk88@gmail.com>
2022-12-04 13:27:47 +0300

    API sorunu giderilemedi

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "39700e946amsh0dd686a2c9deaefp118878jsn64ee8ae08cd2",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "9bc7e575ffe02ccfd7df471b0fa38737";
